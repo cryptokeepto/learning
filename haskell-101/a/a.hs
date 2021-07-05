003e60b06a46eecdc6b8017706f9356dcbf307c6
cryptokeepto <cryptokeepto@gmail.com>
2021-07-05 16:59:24 +0700

    Try condition + guard

--- haskell-101/a/a.hs
+++ haskell-101/a/a.hs
@@ -12,6 +12,7 @@ double x = x * 2
 plus2 :: Int -> Int -> Int
 plus2 x y = x + y + 2
 
+-- Guard condition
 minValue :: Int -> Int -> Int
 minValue a b
     | a < b       = a
@@ -26,4 +27,20 @@ minThreeValue a b c
     | b < c             = b
     | otherwise         = c
 
--- if 3 >= 10 then 1 else 0
+-- Local Definition
+calc :: Float -> Float -> Float -> Float -> Float
+calc x1 y1 x2 y2 = sqrt(dxSq + dySq)
+    where
+      dx = (x2 - x1)
+      dy = (y2 - y1)
+      dxSq = dx ^ 2
+      dySq = dy ^ 2
+
+countEvenNumber :: Int -> Int -> Int -> Int
+countEvenNumber a b c
+    | (isEven a) && (isEven b) && (isEven c)                                      = 3
+    | (isEven a && isEven b) || (isEven a && isEven c) || (isEven b && isEven c)  = 2
+    | isEven a || isEven b || isEven c                                            = 1
+    | otherwise                                                                   = 0
+    where
+      isEven x = mod x 2 == 0
